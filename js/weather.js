46fcd70894fa0a21be0dc9e21fb17d726d068307
xoxun <00xxs2@naver.com>
2023-07-23 21:00:24 +0900

    --course done--

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,15 @@ function onGeoOk(position){
     const lon = position.coords.longitude; //경도
     console.log(`you live in ${lat} ,${lon} `);
     const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url);
+    fetch(url).then(response =>response.json()).then(data=>{
+        const weatherContainer = document.querySelector("#weather span:first-child");
+        const cityContainer = document.querySelector("#weather span:last-child");
+        
+        cityContainer.innerText = data.name;
+        weatherContainer.innerText = "날씨: "+data.weather[0].main+" / 기온:"+data.main.temp;
+        //console.log(data.name, data.weather[0].main);
+    });
+    
 }
 
 function onGeoError(){
